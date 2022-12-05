a5503bf66f79b8834204afe2baa21ffc215eae99
Katherine <katherineleefoley@gmail.com>
2022-12-04 22:39:39 -0600

    deleted my stupid thing tried to add another stupid thing

--- js/app.js
+++ js/app.js
@@ -4,3 +4,5 @@ console.log('sup nerds');
 let userName = prompt('Hi new friend, what is your name?');
 console.log('who the heck are you? ', userName);
 
+let doTheConfirm = confirm('Are you sure that is your REAL name?');
+
